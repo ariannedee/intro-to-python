58f8ef12e53c5e78a8ca149f1e0a4d55455d4ab7
ariannedee <ariannedee@gmail.com>
2019-06-26 19:39:52 -0700

    Fix unnecessary casting to float

--- Problems/problem_6_lucky_number_guess.py
+++ Problems/problem_6_lucky_number_guess.py
@@ -8,7 +8,7 @@ Then say what the answer was.
 import random
 answer = random.randint(1, 10)
 
-guess = int(float(input("I'm thinking of a number between 1 and 10: ")))
+guess = int(input("I'm thinking of a number between 1 and 10: "))
 
 # If the number is correct, tell the user
 # Otherwise, tell them if the answer is higher or lower than their guess
