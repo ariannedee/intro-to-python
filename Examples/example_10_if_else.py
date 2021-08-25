3bf47d1d5d1efaa46fbc2f6038971c12d5eca816
ariannedee <ariannedee@gmail.com>
2021-08-25 13:06:37 -0700

    Uncomment an example

--- Examples/example_10_if_else.py
+++ Examples/example_10_if_else.py
@@ -1,10 +1,10 @@
-# salutation = 'Hello world'
-# shouting = False
-#
-# if shouting:
-#     print(salutation.upper())
-# else:
-#     print(salutation)
+salutation = 'Hello world'
+shouting = False
+
+if shouting:
+    print(salutation.upper())
+else:
+    print(salutation)
 
 
 hungry = True
