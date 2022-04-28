4703ab0cc96e055f7bcc980c3015dff793075564
ariannedee <ariannedee@gmail.com>
2022-04-28 08:37:19 -0700

    Update example

--- Examples/example_5_functions.py
+++ Examples/example_5_functions.py
@@ -2,12 +2,13 @@ def add(a, b):
     return a + b
 
 
-def say_hello(name, capitalize=False):
-    if capitalize:
-        name = name.capitalize()
-    return 'Hello ' + name
+def say_hello(name, shout=False):
+    greeting = 'Hello ' + name
+    if shout:
+        greeting = greeting.upper()
+    return greeting
 
 
 print(say_hello('beyoncé'))
 print(say_hello('beyoncé', True))
-print(say_hello(name='beyoncé', capitalize=True))
+print(say_hello(name='beyoncé', shout=True))
