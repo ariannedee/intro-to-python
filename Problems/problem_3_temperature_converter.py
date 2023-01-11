b111c4c263a93dee5ccd25b483173ca11776bae2
ariannedee <ariannedee@gmail.com>
2023-01-11 11:00:09 -0800

    Initialize with some code

--- Problems/problem_3_temperature_converter.py
+++ Problems/problem_3_temperature_converter.py
@@ -2,7 +2,10 @@
 Given a temperature in Fahrenheit, return the temperature in Celsius
 """
 # Ask for a temperature in Fahrenheit
+temp_f = input("Temp in F: ")
 
 # Calculate it in Celsius
+temp_c = None
 
 # Tell the user what it is
+print("Temp in C: ")
