ce29eacec68965ec532fcb58cf570f07ba5118ce
ariannedee <ariannedee@gmail.com>
2024-06-05 14:06:38 -0700

    Add isinstance examples

--- Examples/example_2_types.py
+++ Examples/example_2_types.py
@@ -7,3 +7,8 @@ print(type(None))
 print(type([1, 2, 10]))
 print(type({'apple': 'A round fruit', 'banana': 'A long yellow fruit',
             'cucumber': 'A long green fruit'}))
+
+print(isinstance(1, int))
+print(isinstance(1, float))
+print(isinstance(1, (int, float)))
+print(isinstance('1', (int, float)))
